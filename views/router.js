28fa7fcac8d298f429b259361f8a057737967bf6
Kelly Su <yoyo420303@gmail.com>
2015-04-15 13:33:31 +0800

    ui-view and moving files from view to public

--- views/router.js
+++ views/router.js
@@ -1,22 +1,59 @@
-var index = angular.module('index', ['ngRoute']);
+var index = angular.module('index', ['ui.router']);
 
-index.config(['$routeProvider', '$locationProvider', function($routeProvider,$locationProvider){
+// , ['ngRoute']
+// index.config(['$routeProvider', '$locationProvider', function($routeProvider,$locationProvider){
 
-	$routeProvider
-		.when('/',{
-			templateUrl: 'template/home.html'
-		})
-		.when('/menu',{
-			templateUrl: 'template/menu.html'
-		})
-		.otherwise({
-			redirectTo: '/'
-		})
-    //$locationProvider.html5Mode(true);
-}]);
+// 	$routeProvider
+// 		.when('/',{
+// 			templateUrl: 'template/home.html'
+// 		})
+// 		.when('/menu',{
+// 			templateUrl: 'template/menu.html'
+// 		})
+// 		.otherwise({
+// 			redirectTo: '/'
+// 		})
+//     $locationProvider.html5Mode(true);
+// }]);
 
 // index.run(['$location', '$rootScope', function($location, $rootScope) {
 //     $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
 //         $rootScope.title = current.$route.title;
 //     });
 // }]);
+
+index.config(function($stateProvider, $urlRouterProvider) {
+  
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/home");
+  
+  // Now set up the states
+  $stateProvider
+    .state('home', {
+      url: "/home",
+      templateUrl: "template/home.html"
+    })
+    // .state('state1.list', {
+    //   url: "/list",
+    //   templateUrl: "partials/state1.list.html",
+    //   controller: function($scope) {
+    //     $scope.items = ["A", "List", "Of", "Items"];
+    //   }
+    // })
+    .state('menu', {
+      url: "/menu",
+      templateUrl: "template/menu.html"
+    })
+    // .state('state2.list', {
+    //   url: "/list",
+    //   templateUrl: "partials/state2.list.html",
+    //   controller: function($scope) {
+    //     $scope.things = ["A", "Set", "Of", "Things"];
+    //   }
+    // });
+	.state('aboutus', {
+      url: "/aboutus",
+      templateUrl: "template/aboutus.html"
+    });
+});
+
